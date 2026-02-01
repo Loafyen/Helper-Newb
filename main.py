cd0867050be7484925fa52d068207959910c1d3f
Loafyen <112283156+Loafyen@users.noreply.github.com>
2026-01-31 19:10:47 -0600

    stupid bot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,9 +11,17 @@ bot = commands.Bot(command_prefix="?", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    await bot.load_extension("tickets")
-    await bot.load_extension("fun")
-    await bot.load_extension("logs")
 
-keep_alive()
+@bot.command()
+async def test(ctx):
+    await ctx.send("hi")
+
+# Load command files
+async def load():
+    await bot.load_extension("tickets")
+    await bot.load_extension("logs")
+    await bot.load_extension("fun")
+
+bot.setup_hook = load
+
 bot.run(os.getenv("TOKEN"))
