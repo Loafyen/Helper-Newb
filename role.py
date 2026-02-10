c6a5f5456fd7b7c149ba91d83561a355c30316ec
Loafyen <112283156+Loafyen@users.noreply.github.com>
2026-02-09 19:18:11 -0600

    Update OWNER_ID to current user ID

--- role.py
+++ role.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-OWNER_ID = 123456789012345678  # <-- replace with YOUR user ID
+OWNER_ID = 918628339663634492  # me
 
 def is_owner_or_perm(**perms):
     async def predicate(ctx):
