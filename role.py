50284ab3c861ec27268d0f91f4c2d0a30dc1e939
Loafyen <112283156+Loafyen@users.noreply.github.com>
2026-02-09 21:58:23 -0600

    Change OWNER_ID to a set of IDs

--- role.py
+++ role.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-OWNER_ID = 918628339663634492  # me
+OWNER_ID = {918628339663634492, 1424568124136624148}  # me
 
 def is_owner_or_perm(**perms):
     async def predicate(ctx):
